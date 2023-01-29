789cd64c6ce63b22c910c80b9aee211beba02e67
marie-george <maria85night@gmail.com>
2023-01-29 17:41:47 +0300

    added upper_first_letters function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 def upper_all_letters(sentence):
+    """Перевести все буквы в верхний регистр"""
     converted = sentence.upper()
+    return converted
+
+
+def upper_first_letters(sentence):
+    """Перевести первые буквы в верхний регистр"""
+    converted = sentence.title()
     return converted
